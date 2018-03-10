e5ce55b22a6a9c2ca7152ada3fce110ff68f5443
temi4000 <mihai.temian@gmail.com>
2018-03-10 15:52:31 +0200

    register + login

--- Foodbox/authentication/views.py
+++ Foodbox/authentication/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from forms import LoginForm, UserRegisterForm
+from django.contrib.auth import logout, authenticate, login
 
-# Create your views here.
+
+def login_view(request):
+    if request.user.is_authenticated():
+        return redirect('/')
+    else:
+        errors = []
+        form = LoginForm(request.POST)
+        if request.method == 'POST':
+            if form.is_valid():
+                user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password'])
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+                else:
+                    errors.append('Incorrect email or password')
+            else:
+                errors.append('Form is not valid')
+        return render(request, "login.html", {
+            'form':form,
+            'errors':errors
+        })
+
+
+def register_view(request):
+    form = UserRegisterForm(data=request.POST or None)
+    errors = []
+    if request.method == 'POST':
+        if form.is_valid() == True:
+            form.instance.set_password(form.cleaned_data['password'])
+            form.save()
+            user = authenticate(username=form.instance.username,
+                                password=form.cleaned_data['password'])
+            login(request, user)
+            return redirect('/')
+    return render(request, "register.html", {
+        'form': form,
+        'errors': errors
+    })
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
